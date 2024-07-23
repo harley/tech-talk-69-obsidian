e35221ab03f2be1be97d581a51465945a37d309b
Harley Trung <harley@coderpush.com>
2024-07-23 18:04:47 +0700

    use giscus for comments

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'harley/tech-talk-69-obsidian',
+        // from data-repo-id
+        repoId: 'R_kgDOMaAtRQ',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOMaAtRc4ChGV1',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/harley/tech-talk-69-obsidian",

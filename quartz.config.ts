54fe4981968532e6808d307453e1f3e4e43d0729
Harley Trung <harley@coderpush.com>
2024-07-23 20:48:29 +0700

    update baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://obsidian.coderdone.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
